24b0f27124baf1807cb8a8c8644d9ecc7ca810bf
Ethanosaure <ethandiasmarques.pro@gmail.com>
2024-03-05 15:31:17 +0100

    starting to add text to pages

---
+++ src/components/Buttons/ExploreBtn/index.tsx
@@ -0,0 +1,11 @@
+import Heading from "../../../typographies/Headings";
+
+const Component = () => {
+  return (
+    <button type="button" className="bg-white">
+      <Heading kind="h4" content={"EXPLORE"} css="" />
+    </button>
+  );
+};
+
+export default Component;
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,24 @@
+import Paragraph from "../../typographies/Paragraphs";
+import Heading from "../../typographies/Headings";
+import ExploreBtn from "../../components/Buttons/ExploreBtn";
+
+import datas from "../../data/data.json";
+
 const Component = () => {
+  const data = datas.home;
+  const subHeadingContent = data.subheading.toUpperCase();
+  const textContent = data.text;
+  const titleContent = data.title.toUpperCase();
   return (
-    <div className="h-screen w-screen bg-[url('../../../public/assets/home/background-home-mobile.jpg')] bg-cover bg-center bg-no-repeat  pt-[100px] md:bg-[url('../../../public/assets/home/background-home-tablet.jpg')] xl:bg-[url('../../../public/assets/home/background-home-desktop.jpg')]">
-      <a href="crew" className="text-white">
-        go to crew Page
-      </a>
+    <div className="flex h-screen w-screen flex-row items-center justify-center bg-[url('../../../public/assets/home/background-home-mobile.jpg')] bg-cover bg-center bg-no-repeat  pt-[100px] md:bg-[url('../../../public/assets/home/background-home-tablet.jpg')] xl:bg-[url('../../../public/assets/home/background-home-desktop.jpg')]">
+      <div className="flex flex-col text-center">
+        <Paragraph kind="span" content={subHeadingContent} css="text-white " />
+        <Heading kind="h1" content={titleContent} css="text-white" />
+        <Paragraph kind="p" content={textContent} css="text-white" />
+      </div>
+      <div>
+        <ExploreBtn />
+      </div>
     </div>
   );
 };
