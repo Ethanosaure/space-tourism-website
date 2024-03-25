c273656c32e01b5fe6bbbd60069685a189e74fd9
Ethanosaure <ethandiasmarques.pro@gmail.com>
2024-03-25 19:37:36 +0100

    adding error page

---
+++ src/components/ReturnBtn/index.tsx
@@ -0,0 +1,21 @@
+import Heading from "../../typographies/Heading";
+
+const Component = ({ css }: { css: string }) => {
+  const pages = ["destination", "crew", "technology"];
+  function randomPageIndex(): number {
+    return Math.floor(Math.random() * pages.length);
+  }
+  function handleClick(): void {
+    const randomPages = randomPageIndex();
+    const randomPage = pages[randomPages];
+    window.location.href = randomPage;
+  }
+
+  return (
+    <button type="button" className="" onClick={handleClick}>
+      <Heading kind="h4" content={"return somewhere safer"} css={css} />
+    </button>
+  );
+};
+
+export default Component;
--- src/pages/ErrorPage/index.tsx
+++ src/pages/ErrorPage/index.tsx
@@ -1,7 +1,29 @@
+import datas from "../../data/data.json";
+import Heading from "../../typographies/Heading";
+import ReturnBtn from "../../components/ReturnBtn";
 const Component = () => {
+  const destinations = datas.destinations.length;
+  function randomPageIndex(): number {
+    return Math.floor(Math.random() * destinations);
+  }
+  const randomPage = randomPageIndex();
+  const planetPictures = datas.destinations[randomPage].images.png;
   return (
-    <div id="error-page">
-      <p>Page not found</p>
+    <div
+      id="error-page"
+      className="flex h-screen min-h-[500px] w-screen flex-col items-center justify-center bg-[url('../../../public/assets/destination/background-destination-mobile.jpg')] bg-cover bg-center bg-no-repeat  md:min-h-[550px] md:bg-[url('../../../public/assets/destination/background-destination-tablet.jpg')] xl:min-h-[630px] xl:bg-[url('../../../public/assets/destination/background-destination-desktop.jpg')] "
+    >
+      <Heading
+        kind="h1"
+        content={"Page not Found"}
+        css=" text-tropicalBlue text-[60px] md:text-[80px] "
+      />
+      <ReturnBtn css="text-white hover:text-tropicalBlue" />
+      <img
+        src={planetPictures}
+        alt="moon picture"
+        className="w-[300px] md:w-[400px] xl:w-[500px]"
+      />
     </div>
   );
 };
